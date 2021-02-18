748c58568392c30771d4dcfd1fdf8d6f5f36ccb4
Ashish Ghosh <Ashish.Ghosh2@ing.com>
2021-02-18 21:22:33 +0100

    First Commit

--- Tests/test_users.py
+++ Tests/test_users.py
@@ -14,11 +14,13 @@ def test_fetch_user() :
 
 
 def test_create_delete_user() :
+    file = open('TestData/user.json',"r")
     path = "api/users"
-    response = requests.post(url=baseUrl+path,json=json.loads('{"name": "morpheus","job": "leader"}'))
+    inputData = json.loads(file.read())
+    response = requests.post(url=baseUrl+path,json=inputData)
     responseJson = json.loads(response.text)
     assert response.status_code == 201
-    assert jsonpath.jsonpath(responseJson,'$.name')[0] == 'morpheus'
+    assert jsonpath.jsonpath(responseJson,'$.name')[0] == inputData["name"]
     id = jsonpath.jsonpath(responseJson,'$.id')[0]
     response = requests.delete(url=baseUrl+path+'/'+id)
     assert response.status_code == 204
